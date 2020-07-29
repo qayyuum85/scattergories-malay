70b25aac4072acff21fbab3c7584c399bbc44596
Qayyuum Harun <qayyuum@logit.global>
2020-07-29 15:44:01 +0800

    Added editing categories

--- src/components/admin/EditWord.jsx
+++ src/components/admin/EditWord.jsx
@@ -16,18 +16,43 @@ const EditWord = () => {
         getWords();
     }, []);
 
+    const handleChange = (event, id) => {
+        console.log(event.target.name, event.target.value, id);
+
+        const editedWordIdx = words.findIndex((word)=>{
+            return word.word_id === id
+        })
+
+        const copy = [...words];
+
+        copy[editedWordIdx][event.target.name] = event.target.value;
+
+        setWords(copy);
+    }
+
     return (
         <div style={editWordContainer}>
+            <div style={headerCatStyle}>Category</div>
             <div style={headerCatStyle}>Text</div>
             <div style={headerScoreStyle}>Score</div>
             <div style={headerEditStyle}>Edit</div>
             {words.map((word) => {
-                const { word_id: id, text, score } = word;
+                const { word_id: id, text, score, category } = word;
                 return (
                     <React.Fragment key={id}>
-                        <div style={bodyCatStyle}>{text}</div>
-                        <div style={bodyScoreStyle}>{score}</div>
-                        <div style={bodyEditStyle}></div>
+                        <div style={bodyCatStyle}>
+                            {category.name}
+                        </div>
+                        <div style={bodyCatStyle}>
+                            <input type="text" name="text" id="" value={text}  onChange={(e) => handleChange(e, id)}/>
+                        </div>
+                        <div style={bodyScoreStyle}>
+                            <input type="text" name="score" id="" value={score}  onChange={(e) => handleChange(e, id)}/>
+                        </div>
+                        <div style={bodyEditStyle}>
+                            <button type="button">Save</button>
+                            <button type="button">Remove</button>
+                        </div>
                     </React.Fragment>
                 );
             })}
@@ -39,24 +64,21 @@ const editWordContainer = {
     flex: "1 1",
     overflowY: "auto",
     display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
+    gridTemplateColumns: "repeat(4, 1fr)",
     gridTemplateRows: "30px auto",
-    gridGap: "10px"
+    gridGap: "10px",
 };
-const headerCatStyle = {
-};
-const headerScoreStyle = {
-};
-const headerEditStyle = {
-};
+const headerCatStyle = {};
+const headerScoreStyle = {};
+const headerEditStyle = {};
 const bodyCatStyle = {
-    height: "30px"
+    height: "30px",
 };
 const bodyScoreStyle = {
-    height: "30px"
+    height: "30px",
 };
 const bodyEditStyle = {
-    height: "30px"
+    height: "30px",
 };
 
 export default EditWord;
